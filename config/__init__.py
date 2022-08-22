c7ea14904edd1dc71cd85295b0c3cb7512d5dd8d
VUONGNGUYEN\Admin <nguyenvuonghlnb@gmail.com>
2022-08-22 11:57:26 +0700

    hix

--- config/__init__.py
+++ config/__init__.py
@@ -3,13 +3,16 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-# path = "./config/." + os.getenv("env") + ".env"
+# path = "./config/." + os.getenv("app_env") + ".env"
 # load_dotenv(dotenv_path=path)
+
+path = f"./config/.{os.getenv('env')}.env"
+load_dotenv(dotenv_path=path)
 
 # print(f"Running config path: {path} {os.environ}")
 
-def load_os_dotenv() :
-    path = Path(f'{os.getcwd()}/config/.{os.environ.get("app_env")}.env')
-    load_dotenv(path)
-    if os.path.exists(path):
-        load_dotenv(path)
+# def load_os_dotenv() :
+#     path = Path(f'{os.getcwd()}/config/.{os.environ.get("app_env")}.env')
+#     load_dotenv(path)
+#     if os.path.exists(path):
+#         load_dotenv(path)
--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,9 @@
 import os
 import psycopg2
-from config import load_os_dotenv
+# from config import load_os_dotenv
 
 # print(os.getenv("POSTGRES_PASS"))
-load_os_dotenv()
+# load_os_dotenv()
 main = psycopg2.connect(
     host=os.getenv("POSTGRES_HOST"),
     port=os.getenv("POSTGRES_PORT"),
